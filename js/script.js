6758bf7dac7b4220c8b2ed9850a3bad0e0a318f3
yafa-pharmacies <khaled7ajsan@gmail.com>
2026-09-22 13:45:50 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,18 +1,3 @@
-function toggleTheme() {
-  document.body.classList.toggle('dark-mode');
-  if (document.body.classList.contains('dark-mode')) {
-    localStorage.setItem('theme', 'dark');
-  } else {
-    localStorage.setItem('theme', 'light');
-  }
-}
-
-window.addEventListener('DOMContentLoaded', function() {
-  if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-mode');
-  }
-});
-
 function hideAll() {
   document.getElementById('landing').style.display = 'none';
   document.querySelectorAll('.product-section').forEach(function(sec) {
